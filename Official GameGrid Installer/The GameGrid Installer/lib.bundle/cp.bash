3163fe49c86f882aa371cc471de58f2d7a4d2097
Adi <adihaya@me.com>
2013-12-30 20:02:17 -0800

    NEW MAC OS INSTALLER!!!

---
+++ Official GameGrid Installer/The GameGrid Installer/lib.bundle/cp.bash
@@ -0,0 +1,9 @@
+file="/work/file.txt"
+
+    for j in `cat $file`
+
+    do
+
+      cp ${j} /destination/direc
+
+    done
--- Official GameGrid Installer/raw dmg files/lib.bundle/cp.bash
+++
@@ -1,9 +0,0 @@
-file="/work/file.txt"
-
-    for j in `cat $file`
-
-    do
-
-      cp ${j} /destination/direc
-
-    done
