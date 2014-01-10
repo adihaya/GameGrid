20d4cb3947388f6f30a24db2d9299288a8a1127c
Adi <adihaya@me.com>
2014-01-09 17:26:27 -0800

    minor updates

--- RbGem/GameGrid/lib/Official GameGrid Installer/GameGrid Installer/lib.bundle/plgvaldb.rb
+++
@@ -1,5 +0,0 @@
-{
-    "install"=>"puts 'The GG Install plugin is located in the install.rb file. This is the starter plugin for GameGrid.';",
-    "starterplugin"=>"puts 'Hello world -- Starter plugin'"
-    }
-
